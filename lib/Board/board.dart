d7cc79f1e79f975541254816a8a303c9d17e361b
Mimand3r <hbecker89@aol.de>
2022-05-29 11:54:05 +0200

    added basic cell & board Code

---
+++ lib/Board/board.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'package:str8tex_frontend/Board/cell.dart' show Cell;
+
+class Board extends StatefulWidget {
+  final int size;
+
+  const Board({Key? key, this.size = 9}) : super(key: key);
+
+  @override
+  State<Board> createState() => _BoardState();
+}
+
+class _BoardState extends State<Board> {
+  @override
+  void didChangeDependencies() {
+    var deviceWidth = MediaQuery.of(context).size.width;
+    cellSize = deviceWidth / widget.size;
+    super.didChangeDependencies();
+  }
+
+  double? cellSize;
+
+  @override
+  Widget build(BuildContext context) {
+    if (cellSize == null) return Container();
+
+    return GridView.count(
+      crossAxisCount: widget.size,
+      children: List.generate(
+        widget.size * widget.size,
+        (index) => Cell(
+          cellSize: cellSize as double,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Board/cell.dart
@@ -0,0 +1,60 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class Cell extends StatefulWidget {
+  final double cellSize;
+
+  const Cell({Key? key, required this.cellSize}) : super(key: key);
+
+  @override
+  State<Cell> createState() => _CellState();
+}
+
+class _CellState extends State<Cell> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: widget.cellSize,
+      height: widget.cellSize,
+      decoration: BoxDecoration(
+          color: const Color(0xFF1b2136),
+          borderRadius: BorderRadius.circular(10 * widget.cellSize / 100),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.7),
+              blurRadius: 2 * widget.cellSize / 100,
+              offset:
+                  Offset(2 * widget.cellSize / 100, 3 * widget.cellSize / 100),
+            )
+          ]),
+      child: Center(
+        child: Container(
+          width: 80 * widget.cellSize / 100,
+          height: 80 * widget.cellSize / 100,
+          decoration: BoxDecoration(
+            color: const Color(0xFF35426b),
+            borderRadius: BorderRadius.circular(2 * widget.cellSize / 100),
+          ),
+          child: Center(
+              child: Text(
+            "9",
+            style: TextStyle(
+              color: const Color(0xFF8a9bd9),
+              fontSize: 70 * widget.cellSize / 100,
+              fontWeight: FontWeight.bold,
+              shadows: [
+                Shadow(
+                  color: Colors.black.withOpacity(0.6),
+                  offset: Offset(
+                      3 * widget.cellSize / 100, 3 * widget.cellSize / 100),
+                  blurRadius: 4 * widget.cellSize / 100,
+                ),
+              ],
+            ),
+          )),
+        ),
+      ),
+    );
+  }
+}
