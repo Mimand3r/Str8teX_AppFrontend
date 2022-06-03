c306220f97b76a0dae1e2eba0634a9462f34163d
Mimand3r <hbecker89@aol.de>
2022-06-03 14:32:56 +0200

    enabled firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:str8tex_frontend/Board/board_page.dart';
 import 'package:str8tex_frontend/Board/board_state.dart';
+import 'package:str8tex_frontend/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     ChangeNotifierProvider(
       create: (context) => BoardStateProvider(),
